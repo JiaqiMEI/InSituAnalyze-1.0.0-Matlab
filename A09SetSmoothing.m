b610d17f237ed50a7ef5819814d27f393c3399ea
JiaqiMEI <38342646+JiaqiMEI@users.noreply.github.com>
2018-04-17 11:02:41 +0800

    Update A09SetSmoothing.m

--- A09SetSmoothing.m
+++ A09SetSmoothing.m
@@ -24,6 +24,24 @@ function varargout = A09SetSmoothing(varargin)
 
 % Last Modified by GUIDE v2.5 19-Jan-2018 12:40:41
 
+
+
+% Copyright [Jiaqi MEI, China Agricultural University]
+
+  %   Licensed under the Apache License, Version 2.0 (the "License");
+  %   you may not use this file except in compliance with the License.
+  %   You may obtain a copy of the License at
+
+  %   http://www.apache.org/licenses/LICENSE-2.0
+
+  %   Unless required by applicable law or agreed to in writing, software
+  %   distributed under the License is distributed on an "AS IS" BASIS,
+  %   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  %   See the License for the specific language governing permissions and
+  %   limitations under the License.  
+  
+  
+
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
@@ -52,7 +70,7 @@ function A09SetSmoothing_OpeningFcn(hObject, eventdata, handles, varargin)
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to A09SetSmoothing (see VARARGIN)
 
-% ¥∞ø⁄æ”÷–
+% ¬¥¬∞¬ø√ö¬æ√ì√ñ√ê
 % set(hObject,'Units','pixels');
 % figuresize=get(hObject,'Position');
 % screensize=get(0,'screensize');
