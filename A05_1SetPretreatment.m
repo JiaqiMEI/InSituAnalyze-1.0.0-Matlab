cfe91701910001a79789c1ada44533b410555d5d
JiaqiMEI <38342646+JiaqiMEI@users.noreply.github.com>
2018-04-17 10:55:13 +0800

    Update A05_1SetPretreatment.m

--- A05_1SetPretreatment.m
+++ A05_1SetPretreatment.m
@@ -24,6 +24,24 @@ function varargout = A05_1SetPretreatment(varargin)
 
 % Last Modified by GUIDE v2.5 29-Jan-2018 13:39:22
 
+
+
+% Copyright [Jiaqi MEI, China Agricultural University]
+
+  %   Licensed under the Apache License, Version 2.0 (the "License");
+  %   you may not use this file except in compliance with the License.
+  %   You may obtain a copy of the License at
+
+  %   http://www.apache.org/licenses/LICENSE-2.0
+
+  %   Unless required by applicable law or agreed to in writing, software
+  %   distributed under the License is distributed on an "AS IS" BASIS,
+  %   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  %   See the License for the specific language governing permissions and
+  %   limitations under the License.  
+  
+  
+
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
